a85821b24b42c5ca83d04e8f5cac10c3bb57331a
Joao Vitor <githubconta2021@gmail.com>
2024-05-01 13:40:09 -0300

    Descirption: criando model de funcionario com herança do cliente

---
+++ ControleVendasEstoque/br.com.projeto.model/Funcionario.cs
@@ -0,0 +1,9 @@
+﻿namespace ControleVendasEstoque.br.com.projeto.model
+{
+    public class Funcionario : Cliente
+    {
+        public string Senha { get; set; }
+        public string Cargo { get; set; }
+        public string NivelAcesso { get; set; }
+    }
+}
