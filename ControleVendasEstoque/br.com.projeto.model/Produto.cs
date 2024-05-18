76724cbf9b7cab6fbf5a13652011142c6c78d0e2
Joao Vitor <githubconta2021@gmail.com>
2024-05-18 18:22:09 -0300

    Description: criando o model produtos com chave estrageira do Forncedor no Produto

---
+++ ControleVendasEstoque/br.com.projeto.model/Produto.cs
@@ -0,0 +1,11 @@
+﻿namespace ControleVendasEstoque.br.com.projeto.model
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public int Qtdestoque { get; set; }
+        public int for_id { get; set; }
+    }
+}
