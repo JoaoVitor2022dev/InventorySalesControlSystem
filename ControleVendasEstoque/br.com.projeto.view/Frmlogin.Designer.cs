c90c13b0a571d6db8132b01732e62a1ca0e3b8e3
Joao Vitor <githubconta2021@gmail.com>
2024-07-21 21:32:40 -0300

    Description: criando Menu de Controle de Sistema 1 - colcoando imagem de logos de opções; 2 - desenvolvendo a barra de status de hora e data e usuario;

---
+++ ControleVendasEstoque/br.com.projeto.view/Frmlogin.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace ControleVendasEstoque.br.com.projeto.view
+{
+    partial class Frmlogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtemail = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Txtpassword = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnentrar = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.ForeColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(6);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(749, 157);
+            this.panel1.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(161, 61);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(463, 41);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Authenticação de Sistema";
+            // 
+            // txtemail
+            // 
+            this.txtemail.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtemail.Location = new System.Drawing.Point(234, 212);
+            this.txtemail.Name = "txtemail";
+            this.txtemail.Size = new System.Drawing.Size(342, 31);
+            this.txtemail.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(152, 215);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 24);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "E-mail:";
+            // 
+            // Txtpassword
+            // 
+            this.Txtpassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Txtpassword.Location = new System.Drawing.Point(234, 294);
+            this.Txtpassword.Name = "Txtpassword";
+            this.Txtpassword.PasswordChar = '*';
+            this.Txtpassword.Size = new System.Drawing.Size(342, 31);
+            this.Txtpassword.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(140, 294);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 24);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Cliente:";
+            // 
+            // btnentrar
+            // 
+            this.btnentrar.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnentrar.Font = new System.Drawing.Font("Century Gothic", 12F);
+            this.btnentrar.ForeColor = System.Drawing.Color.White;
+            this.btnentrar.Location = new System.Drawing.Point(284, 394);
+            this.btnentrar.Name = "btnentrar";
+            this.btnentrar.Size = new System.Drawing.Size(180, 48);
+            this.btnentrar.TabIndex = 21;
+            this.btnentrar.Text = "Entrar";
+            this.btnentrar.UseVisualStyleBackColor = false;
+            // 
+            // Frmlogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(749, 476);
+            this.Controls.Add(this.btnentrar);
+            this.Controls.Add(this.Txtpassword);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtemail);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel1);
+            this.Name = "Frmlogin";
+            this.Text = "Login de Usúario";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.TextBox txtemail;
+        private System.Windows.Forms.Label label3;
+        public System.Windows.Forms.TextBox Txtpassword;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnentrar;
+    }
+}
---
+++ ControleVendasEstoque/br.com.projeto.view/Frmmenu.Designer.cs
@@ -0,0 +1,321 @@
+﻿namespace ControleVendasEstoque.br.com.projeto.view
+{
+    partial class FrmMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.Menucliente = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtmenuclientecadastrar = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtmenuclienteconsulta = new System.Windows.Forms.ToolStripMenuItem();
+            this.Clientfuncionario = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtmenuFuncionarioCadastrar = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtmenufuncionarioconsulta = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuFornecedor = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtmenuFornecedorCadastrar = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtmenuFornecedorConsulta = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuProdutos = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtmenuProdutoCadastrar = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtmenuProdutoConsultar = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuVendas = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtmenuNovaVenda = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtmenuHistoricoVenda = new System.Windows.Forms.ToolStripMenuItem();
+            this.configuração = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtmenuTrocaUsuario = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtmenuSairSistema = new System.Windows.Forms.ToolStripMenuItem();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.txtdata = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.txthora = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel5 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.txtusuario = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.White;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Menucliente,
+            this.Clientfuncionario,
+            this.MenuFornecedor,
+            this.MenuProdutos,
+            this.MenuVendas,
+            this.configuração});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(999, 56);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // Menucliente
+            // 
+            this.Menucliente.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.txtmenuclientecadastrar,
+            this.txtmenuclienteconsulta});
+            this.Menucliente.Image = global::ControleVendasEstoque.Properties.Resources._4308184_client_costs_employee_finance_money_icon;
+            this.Menucliente.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.Menucliente.Name = "Menucliente";
+            this.Menucliente.Size = new System.Drawing.Size(109, 52);
+            this.Menucliente.Text = "Clientes";
+            // 
+            // txtmenuclientecadastrar
+            // 
+            this.txtmenuclientecadastrar.Name = "txtmenuclientecadastrar";
+            this.txtmenuclientecadastrar.Size = new System.Drawing.Size(180, 22);
+            this.txtmenuclientecadastrar.Text = "Cadastrar Cliente";
+            // 
+            // txtmenuclienteconsulta
+            // 
+            this.txtmenuclienteconsulta.Name = "txtmenuclienteconsulta";
+            this.txtmenuclienteconsulta.Size = new System.Drawing.Size(180, 22);
+            this.txtmenuclienteconsulta.Text = "Consultar Cliente";
+            // 
+            // Clientfuncionario
+            // 
+            this.Clientfuncionario.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.txtmenuFuncionarioCadastrar,
+            this.txtmenufuncionarioconsulta});
+            this.Clientfuncionario.Image = global::ControleVendasEstoque.Properties.Resources._2530821_business_man_employee_general_human_member_icon;
+            this.Clientfuncionario.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.Clientfuncionario.Name = "Clientfuncionario";
+            this.Clientfuncionario.Size = new System.Drawing.Size(130, 52);
+            this.Clientfuncionario.Text = "Funcionário";
+            // 
+            // txtmenuFuncionarioCadastrar
+            // 
+            this.txtmenuFuncionarioCadastrar.Name = "txtmenuFuncionarioCadastrar";
+            this.txtmenuFuncionarioCadastrar.Size = new System.Drawing.Size(191, 22);
+            this.txtmenuFuncionarioCadastrar.Text = "Cadastrar Funcionário";
+            // 
+            // txtmenufuncionarioconsulta
+            // 
+            this.txtmenufuncionarioconsulta.Name = "txtmenufuncionarioconsulta";
+            this.txtmenufuncionarioconsulta.Size = new System.Drawing.Size(191, 22);
+            this.txtmenufuncionarioconsulta.Text = "Consultar Funcionário";
+            // 
+            // MenuFornecedor
+            // 
+            this.MenuFornecedor.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.txtmenuFornecedorCadastrar,
+            this.txtmenuFornecedorConsulta});
+            this.MenuFornecedor.Image = global::ControleVendasEstoque.Properties.Resources._4172394_goods_merchandise_stock_supply_vendibles_icon;
+            this.MenuFornecedor.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.MenuFornecedor.Name = "MenuFornecedor";
+            this.MenuFornecedor.Size = new System.Drawing.Size(138, 52);
+            this.MenuFornecedor.Text = "Fornecedores";
+            // 
+            // txtmenuFornecedorCadastrar
+            // 
+            this.txtmenuFornecedorCadastrar.Name = "txtmenuFornecedorCadastrar";
+            this.txtmenuFornecedorCadastrar.Size = new System.Drawing.Size(188, 22);
+            this.txtmenuFornecedorCadastrar.Text = "Cadastrar Fornecedor";
+            // 
+            // txtmenuFornecedorConsulta
+            // 
+            this.txtmenuFornecedorConsulta.Name = "txtmenuFornecedorConsulta";
+            this.txtmenuFornecedorConsulta.Size = new System.Drawing.Size(188, 22);
+            this.txtmenuFornecedorConsulta.Text = "Consultar Fornecedor";
+            // 
+            // MenuProdutos
+            // 
+            this.MenuProdutos.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.txtmenuProdutoCadastrar,
+            this.txtmenuProdutoConsultar});
+            this.MenuProdutos.Image = global::ControleVendasEstoque.Properties.Resources._6843036_courier_deliver_delivery_package_product_icon;
+            this.MenuProdutos.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.MenuProdutos.Name = "MenuProdutos";
+            this.MenuProdutos.Size = new System.Drawing.Size(115, 52);
+            this.MenuProdutos.Text = "Produtos";
+            // 
+            // txtmenuProdutoCadastrar
+            // 
+            this.txtmenuProdutoCadastrar.Name = "txtmenuProdutoCadastrar";
+            this.txtmenuProdutoCadastrar.Size = new System.Drawing.Size(180, 22);
+            this.txtmenuProdutoCadastrar.Text = "Cadastrar Produtos";
+            // 
+            // txtmenuProdutoConsultar
+            // 
+            this.txtmenuProdutoConsultar.Name = "txtmenuProdutoConsultar";
+            this.txtmenuProdutoConsultar.Size = new System.Drawing.Size(180, 22);
+            this.txtmenuProdutoConsultar.Text = "Consultar Produtos";
+            // 
+            // MenuVendas
+            // 
+            this.MenuVendas.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.txtmenuNovaVenda,
+            this.txtmenuHistoricoVenda});
+            this.MenuVendas.Image = global::ControleVendasEstoque.Properties.Resources._465074_cart_purchase_shopping_basket_buy_icon;
+            this.MenuVendas.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.MenuVendas.Name = "MenuVendas";
+            this.MenuVendas.Size = new System.Drawing.Size(104, 52);
+            this.MenuVendas.Text = "Vendas";
+            // 
+            // txtmenuNovaVenda
+            // 
+            this.txtmenuNovaVenda.Name = "txtmenuNovaVenda";
+            this.txtmenuNovaVenda.Size = new System.Drawing.Size(180, 22);
+            this.txtmenuNovaVenda.Text = "Nova Vendas";
+            // 
+            // txtmenuHistoricoVenda
+            // 
+            this.txtmenuHistoricoVenda.Name = "txtmenuHistoricoVenda";
+            this.txtmenuHistoricoVenda.Size = new System.Drawing.Size(180, 22);
+            this.txtmenuHistoricoVenda.Text = "Histórico Vendas";
+            // 
+            // configuração
+            // 
+            this.configuração.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.txtmenuTrocaUsuario,
+            this.txtmenuSairSistema});
+            this.configuração.Image = global::ControleVendasEstoque.Properties.Resources._211670_b_gear_icon;
+            this.configuração.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.configuração.Name = "configuração";
+            this.configuração.Size = new System.Drawing.Size(144, 52);
+            this.configuração.Text = "Configurações";
+            // 
+            // txtmenuTrocaUsuario
+            // 
+            this.txtmenuTrocaUsuario.Name = "txtmenuTrocaUsuario";
+            this.txtmenuTrocaUsuario.Size = new System.Drawing.Size(180, 22);
+            this.txtmenuTrocaUsuario.Text = "Trocar de Usuário";
+            // 
+            // txtmenuSairSistema
+            // 
+            this.txtmenuSairSistema.Name = "txtmenuSairSistema";
+            this.txtmenuSairSistema.Size = new System.Drawing.Size(180, 22);
+            this.txtmenuSairSistema.Text = "Sair do Sistema";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.BackColor = System.Drawing.Color.White;
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.txtdata,
+            this.toolStripStatusLabel3,
+            this.txthora,
+            this.toolStripStatusLabel5,
+            this.txtusuario});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 607);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(999, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(65, 17);
+            this.toolStripStatusLabel1.Text = "Data Atual:";
+            this.toolStripStatusLabel1.Click += new System.EventHandler(this.toolStripStatusLabel1_Click);
+            // 
+            // txtdata
+            // 
+            this.txtdata.Name = "txtdata";
+            this.txtdata.Size = new System.Drawing.Size(65, 17);
+            this.txtdata.Text = "21/07/2024";
+            // 
+            // toolStripStatusLabel3
+            // 
+            this.toolStripStatusLabel3.Name = "toolStripStatusLabel3";
+            this.toolStripStatusLabel3.Size = new System.Drawing.Size(67, 17);
+            this.toolStripStatusLabel3.Text = "Hora Atual:";
+            // 
+            // txthora
+            // 
+            this.txthora.Name = "txthora";
+            this.txthora.Size = new System.Drawing.Size(34, 17);
+            this.txthora.Text = "10:30";
+            // 
+            // toolStripStatusLabel5
+            // 
+            this.toolStripStatusLabel5.Name = "toolStripStatusLabel5";
+            this.toolStripStatusLabel5.Size = new System.Drawing.Size(93, 17);
+            this.toolStripStatusLabel5.Text = "Usuário Logado:";
+            // 
+            // txtusuario
+            // 
+            this.txtusuario.Name = "txtusuario";
+            this.txtusuario.Size = new System.Drawing.Size(59, 17);
+            this.txtusuario.Text = "João Vitor";
+            // 
+            // FrmMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::ControleVendasEstoque.Properties.Resources.logo_menu;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(999, 629);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FrmMenu";
+            this.Text = "Menu Principal - Controle de Vendas";
+            this.Load += new System.EventHandler(this.FrmMenu_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem Menucliente;
+        private System.Windows.Forms.ToolStripMenuItem txtmenuclientecadastrar;
+        private System.Windows.Forms.ToolStripMenuItem txtmenuclienteconsulta;
+        private System.Windows.Forms.ToolStripMenuItem Clientfuncionario;
+        private System.Windows.Forms.ToolStripMenuItem MenuFornecedor;
+        private System.Windows.Forms.ToolStripMenuItem txtmenuFuncionarioCadastrar;
+        private System.Windows.Forms.ToolStripMenuItem txtmenufuncionarioconsulta;
+        private System.Windows.Forms.ToolStripMenuItem MenuProdutos;
+        private System.Windows.Forms.ToolStripMenuItem MenuVendas;
+        private System.Windows.Forms.ToolStripMenuItem configuração;
+        private System.Windows.Forms.ToolStripMenuItem txtmenuFornecedorCadastrar;
+        private System.Windows.Forms.ToolStripMenuItem txtmenuFornecedorConsulta;
+        private System.Windows.Forms.ToolStripMenuItem txtmenuProdutoCadastrar;
+        private System.Windows.Forms.ToolStripMenuItem txtmenuProdutoConsultar;
+        private System.Windows.Forms.ToolStripMenuItem txtmenuNovaVenda;
+        private System.Windows.Forms.ToolStripMenuItem txtmenuHistoricoVenda;
+        private System.Windows.Forms.ToolStripMenuItem txtmenuTrocaUsuario;
+        private System.Windows.Forms.ToolStripMenuItem txtmenuSairSistema;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel txtdata;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel3;
+        private System.Windows.Forms.ToolStripStatusLabel txthora;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel5;
+        private System.Windows.Forms.ToolStripStatusLabel txtusuario;
+    }
+}
